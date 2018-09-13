440a4a7679aba30ccc455e622dd9331c39351ad7
archistico <info@archistico.com>
2018-09-13 23:06:00 +0200

    modifiche /data

--- tmp/2soh81j2kta8c.3kdsmbk4lmwwc.php
+++ tmp/2soh81j2kta8c.3kdsmbk4lmwwc.php
@@ -6,12 +6,12 @@
 
 <ul>
     <?php foreach (($categoria2?:[]) as $item): ?>
-        <li><?= ($item['madre']) ?>: <?= ($item['descrizione']) ?> (<?= ($item['id']) ?>) </li>
+        <li><?= ($item['madre']) ?>/<?= ($item['descrizione']) ?> (<?= ($item['id']) ?>) </li>
     <?php endforeach; ?>
 </ul>
 
 <ul>
     <?php foreach (($categoria3?:[]) as $item): ?>
-        <li><?= ($item['cat1']) ?>/<?= ($item['cat2']) ?>: <?= ($item['descrizione']) ?> (<?= ($item['id']) ?>) </li>
+        <li><?= ($item['cat1']) ?>/<?= ($item['cat2']) ?>/<?= ($item['descrizione']) ?> (<?= ($item['id']) ?>) </li>
     <?php endforeach; ?>
 </ul>
