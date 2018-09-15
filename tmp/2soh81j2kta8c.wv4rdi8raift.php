3125b08b3a806dedec11635df8dc0e405df2d7f6
archistico <info@archistico.com>
2018-09-15 14:52:51 +0200

    conf htaccess

--- tmp/2soh81j2kta8c.wv4rdi8raift.php
+++
@@ -1,8 +0,0 @@
-<div class="row">
-    <div class="col-md-12">
-        <p>Nuovo movimento</p>
-        <?php foreach (($categoria1?:[]) as $item): ?>
-            <a class="btn btn-info btn-block btn-lg" href="<?= (Base::instance()->alias('nuovo')) ?>/<?= ($item['id']) ?>"><?= ($item['descrizione']) ?></a>
-        <?php endforeach; ?>
-    </div>
-</div>
